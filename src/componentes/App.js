21adcaaaa6dbb3ceb982030e649166994406f055
DESKTOP-VKKL5LV\usuario <titanmar15@gmail.com>
2019-06-21 19:33:36 -0500

    first commit

---
+++ src/componentes/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header titulo="Cotizador de Seguro de Auto" />
+    </div>
+  );
+}
+
+export default App;
